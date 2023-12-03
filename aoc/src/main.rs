c65086da6375b5823b9f0055beacee572090d747
Carlo Cosenza <214245+gnome-skillet@users.noreply.github.com>
2023-12-02 20:34:16 -0800

    feat(day2): solution to day2 2023
    solution to day2 2023

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -15,7 +15,7 @@ struct Opts {
 #[enum_dispatch(CommandImpl)]
 #[derive(Parser, Debug)]
 enum SubCommand {
-    Day1(day1::Day1),
+    Day2(day2::Day2),
 }
 fn main() -> Result<(), DynError> {
     let opts = Opts::parse();
