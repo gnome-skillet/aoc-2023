aa614060c55cd9c04de1fc45fc1a02b5b15722d7
Carlo Cosenza <214245+gnome-skillet@users.noreply.github.com>
2023-12-06 16:06:21 -0800

    refactor(day5): replace brute force with range
    replace brute force with range

--- aoc/src/commands/day5.rs
+++ aoc/src/commands/day5.rs
@@ -28,14 +28,15 @@ pub struct Day5 {
     input: PathBuf,
 }
 
-fn parse_range(input: &str) -> IResult<&str, (usize,InclusiveRange)> {
+fn parse_range(input: &str) -> IResult<&str, InclusiveRange> {
     let (input, soil_map) = separated_list1(space1, u32)(input)?;
     let offset: usize = soil_map[0] as usize;
-    let range: InclusiveRange = InclusiveRange::new(soil_map[1] as usize,soil_map[2] as usize);
-    Ok((input, (offset,range)))
+    let range: InclusiveRange =
+        InclusiveRange::new(soil_map[1] as usize, soil_map[2] as usize, offset);
+    Ok((input, range))
 }
 
-fn parse_map(input: &str) -> IResult<&str, Vec<(usize,InclusiveRange)>> {
+fn parse_map(input: &str) -> IResult<&str, Vec<InclusiveRange>> {
     let (input, soil_maps) = separated_list1(newline, parse_range)(input)?;
     Ok((input, soil_maps))
 }
@@ -43,11 +44,15 @@ fn parse_map(input: &str) -> IResult<&str, Vec<(usize,InclusiveRange)>> {
 fn parse_seeds(input: &str) -> IResult<&str, Vec<u32>> {
     let (input, _) = tag("seeds:")(input)?;
     let (input, _) = space1(input)?;
-    let (input, seeds) = separated_list1(space1, u32)(input)?;
+    let (input, mut seeds) = separated_list1(space1, u32)(input)?;
+    let coalesced_seeds: Vec<InclusiveRange> = Vec::new();
     Ok((input, seeds))
 }
 
-pub fn keep(source: &Vec<InclusiveRange>, mymap: (usize,Vec<InclusiveRange>)) -> Vec<InclusiveRange> {
+pub fn keep(
+    source: &Vec<InclusiveRange>,
+    mymap: (usize, Vec<InclusiveRange>),
+) -> Vec<InclusiveRange> {
     todo!()
 }
 
@@ -56,18 +61,31 @@ pub fn get_seeds(seeds: Vec<u32>) -> Vec<InclusiveRange> {
     let mut newseeds: Vec<InclusiveRange> = Vec::new();
     while i < seeds.len() - 1 {
         let start: usize = seeds[i] as usize;
-        let length: usize = seeds[i+1] as usize;
-        let range: InclusiveRange = InclusiveRange::new(start, length);
+        let length: usize = seeds[i + 1] as usize;
+        let range: InclusiveRange = InclusiveRange::new(start, length, 0);
         newseeds.push(range);
         i += 2;
     }
-    newseeds
+    newseeds.sort();
+    let mut coalesced_seeds: Vec<InclusiveRange> = Vec::new();
+    while !newseeds.is_empty() {
+        let mut top = newseeds.pop().unwrap();
+        loop {
+            if !newseeds.is_empty() && top.overlaps(&newseeds[0]) {
+                let newtop = newseeds.pop().unwrap();
+                top = top.coalesce(&newtop).unwrap();
+            } else {
+                coalesced_seeds.push(top);
+                break;
+            }
+        }
+    }
+    coalesced_seeds
 }
 
 fn parse_almanac2(input: &str) -> IResult<&str, usize> {
     let (input, seeds) = parse_seeds(input)?;
-    let mut seeds: Vec<InclusiveRange> = get_seeds(seeds); 
-    println!("seeds: {seeds:#?}");
+    let mut seeds: Vec<InclusiveRange> = get_seeds(seeds);
 
     let (input, _) = newline(input)?;
     let (input, _) = newline(input)?;
@@ -77,26 +95,11 @@ fn parse_almanac2(input: &str) -> IResult<&str, usize> {
     println!("seed_to_soil: {seed_to_soil:#?}");
     let (input, _) = newline(input)?;
     let (input, _) = newline(input)?;
-    let soils: Vec<InclusiveRange> = Vec::new(); 
-    while !seeds.is_empty() {
-        let myrange = seeds.pop().unwrap();
-        //for (offset, range) in seed_to_soil {
-            //let myoverlap = myrange.overlap(range); 
-            //let (mappy, offset) = range.offset();
-            //if Some(myoverlap) = myoverlap {
-            //    soils.push(myoverlap.mapit(mappy,offset));
-            //}
-            //let mydisjunctions = myrange.disjunction(&range); 
-        //}
-    }
-    
-
     let (input, _) = tag("soil-to-fertilizer map:")(input)?;
     let (input, _) = newline(input)?;
     let (input, soil_to_fertilizer) = parse_map(input)?;
     let (input, _) = newline(input)?;
     let (input, _) = newline(input)?;
-    //let numbers: HashSet<u32> = keep(&numbers,soil_to_fertilizer);
 
     let (input, _) = tag("fertilizer-to-water map:")(input)?;
     let (input, _) = newline(input)?;
@@ -138,36 +141,26 @@ fn parse_almanac2(input: &str) -> IResult<&str, usize> {
     Ok((input, min))
 }
 
-#[derive(Debug,Default,Eq)]
+#[derive(Debug, Default, Eq)]
 pub struct InclusiveRange {
     start: usize,
     length: usize,
+    projection: usize,
 }
 
 impl InclusiveRange {
-    pub fn new(start: usize, length: usize) -> Self {
-        InclusiveRange {
-            start: start,
-            length: length,
-        }
+    pub fn new(start: usize, length: usize, projection: usize) -> Self {
+        InclusiveRange { start, length, projection }
     }
 
     pub fn offset(&self, other_start: usize) -> (i8, usize) {
-        let sign = if self.start > other_start {
-            1i8
-        } else {
-            -1i8
-        };
+        let sign = if self.start > other_start { 1i8 } else { -1i8 };
         (sign, self.start.abs_diff(other_start))
     }
 
     pub fn mapit(&self, mappy: (i8, usize)) -> Self {
-        let newstart = if mappy.0 > 0 {
-            self.start + mappy.1
-        } else {
-            self.start - mappy.1
-        };
-        InclusiveRange::new(newstart, self.length)
+        let newstart = if mappy.0 > 0 { self.start + mappy.1 } else { self.start - mappy.1 };
+        InclusiveRange::new(newstart, self.length, self.projection)
     }
 
     // check if there is an overlap between self and other
@@ -177,11 +170,11 @@ impl InclusiveRange {
         end_of_self >= other.start && end_of_other >= self.start
     }
 
-    // check if self contains other 
+    // check if self contains other
     pub fn contains(&self, other: &Self) -> bool {
         let end_of_self: usize = self.start + self.length - 1;
         let end_of_other: usize = other.start + other.length - 1;
-        self.start <= other.start && end_of_other <= end_of_self 
+        self.start <= other.start && end_of_other <= end_of_self
     }
 
     pub fn coalesce(&self, other: &Self) -> Option<Self> {
@@ -189,17 +182,10 @@ impl InclusiveRange {
             return None;
         }
 
-        let start: usize = if self.start < other.start {
-            self.start
-        } else {
-            other.start
-        };
-        let length: usize = if self.end() > other.end() {
-            self.end() - start + 1
-        } else {
-            other.end() - start + 1
-        };
-        Some(InclusiveRange::new(start,length))
+        let start: usize = if self.start < other.start { self.start } else { other.start };
+        let length: usize =
+            if self.end() > other.end() { self.end() - start + 1 } else { other.end() - start + 1 };
+        Some(InclusiveRange::new(start, length, self.projection))
     }
 
     pub fn end(&self) -> usize {
@@ -211,13 +197,15 @@ impl InclusiveRange {
         if self.contains(other) && other.contains(self) {
             return None;
         }
-        let mut nonoverlap: Vec<InclusiveRange> = Vec::new(); 
+        let mut nonoverlap: Vec<InclusiveRange> = Vec::new();
         if self.start < other.start {
-            let newrange: InclusiveRange = InclusiveRange::new(self.start, other.start - self.start);
+            let newrange: InclusiveRange =
+                InclusiveRange::new(self.start, other.start - self.start, self.projection);
             nonoverlap.push(newrange);
         }
         if self.end() > other.end() {
-            let newrange: InclusiveRange = InclusiveRange::new(other.end() + 1, self.end());
+            let newrange: InclusiveRange =
+                InclusiveRange::new(other.end() + 1, self.end(), self.projection);
             nonoverlap.push(newrange);
         }
         Some(nonoverlap)
@@ -226,28 +214,24 @@ impl InclusiveRange {
     // return portion of self that overlaps with other
     pub fn overlap(&self, other: &Self) -> Option<Self> {
         if !self.overlaps(other) {
-            return None 
+            return None;
         }
 
         let end_of_self: usize = self.start + self.length - 1;
         let end_of_other: usize = other.start + other.length - 1;
-        let start_of_overlap = if self.start >= other.start {
-            self.start
-        } else {
-            other.start
-        };
+        let start_of_overlap = if self.start >= other.start { self.start } else { other.start };
         let length_of_overlap = if end_of_self <= end_of_other {
-            end_of_self - start_of_overlap + 1 
+            end_of_self - start_of_overlap + 1
         } else {
-            end_of_other - start_of_overlap + 1 
+            end_of_other - start_of_overlap + 1
         };
-        Some(InclusiveRange::new(start_of_overlap, length_of_overlap))
+        Some(InclusiveRange::new(start_of_overlap, length_of_overlap, self.projection))
     }
 }
 
 impl Ord for InclusiveRange {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.start.cmp(&other.start)
+        other.start.cmp(&self.start)
     }
 }
 
@@ -269,7 +253,6 @@ impl CommandImpl for Day5 {
         let (input, almanac) = parse_almanac2(&string).unwrap();
         println!("min: {almanac}");
 
-
         Ok(())
     }
 }
@@ -283,7 +266,7 @@ mod tests {
         let x: String = "seeds: 79 14 55 13".to_string();
         let input: &str = &x;
         let (input, actual) = parse_seeds(input).unwrap();
-        let expected: Vec<u32> = vec![79,14, 55, 13]; 
+        let expected: Vec<u32> = vec![79, 14, 55, 13];
         assert_eq!(actual, expected);
     }
 
@@ -291,47 +274,49 @@ mod tests {
     fn test_parse_soilmap() {
         let x: String = "0 15 37
 37 52 2
-".to_string();
+"
+        .to_string();
         let input: &str = &x;
         let (input, actual) = parse_map(input).unwrap();
-        let expected: Vec<(usize,InclusiveRange)> = vec![(0,InclusiveRange::new(15,37)), (37,InclusiveRange::new(52,2))]; 
+        let expected: Vec<(usize, InclusiveRange)> =
+            vec![(0, InclusiveRange::new(15, 37)), (37, InclusiveRange::new(52, 2))];
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn test_range_contains() {
-        let this: InclusiveRange = InclusiveRange::new(5,5); 
-        let that: InclusiveRange = InclusiveRange::new(6,3); 
+        let this: InclusiveRange = InclusiveRange::new(5, 5);
+        let that: InclusiveRange = InclusiveRange::new(6, 3);
         assert!(this.contains(&that));
     }
 
     #[test]
     fn test_range_overlaps() {
-        let this: InclusiveRange = InclusiveRange::new(5,5); 
-        let that: InclusiveRange = InclusiveRange::new(9,1); 
+        let this: InclusiveRange = InclusiveRange::new(5, 5);
+        let that: InclusiveRange = InclusiveRange::new(9, 1);
         assert!(this.overlaps(&that));
     }
 
     #[test]
     fn test_range_overlap() {
-        let this: InclusiveRange = InclusiveRange::new(5,5); 
-        let temp: InclusiveRange = InclusiveRange::new(5,5); 
-        let that: InclusiveRange = this.overlap(&temp).unwrap(); 
-        assert_eq!(this,that);
+        let this: InclusiveRange = InclusiveRange::new(5, 5);
+        let temp: InclusiveRange = InclusiveRange::new(5, 5);
+        let that: InclusiveRange = this.overlap(&temp).unwrap();
+        assert_eq!(this, that);
     }
 
     #[test]
     fn test_range_disjunction() {
-        let this: InclusiveRange = InclusiveRange::new(5,5); 
-        let temp: InclusiveRange = InclusiveRange::new(5,5); 
-        let that = this.disjunction(&temp); 
+        let this: InclusiveRange = InclusiveRange::new(5, 5);
+        let temp: InclusiveRange = InclusiveRange::new(5, 5);
+        let that = this.disjunction(&temp);
         assert!(that.is_none());
 
-        let this: InclusiveRange = InclusiveRange::new(4,5); 
-        let temp: InclusiveRange = InclusiveRange::new(5,5); 
-        let that = this.disjunction(&temp).unwrap(); 
-        let expected: InclusiveRange = InclusiveRange::new(4,1); 
-        assert_eq!(that.len(),1);
-        assert_eq!(expected,that[0]);
+        let this: InclusiveRange = InclusiveRange::new(4, 5);
+        let temp: InclusiveRange = InclusiveRange::new(5, 5);
+        let that = this.disjunction(&temp).unwrap();
+        let expected: InclusiveRange = InclusiveRange::new(4, 1);
+        assert_eq!(that.len(), 1);
+        assert_eq!(expected, that[0]);
     }
 }
